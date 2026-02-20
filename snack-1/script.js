7518364994a858742e1ecc0610591fb4b524657e
angeloren92 <angeloren92@gmail.com>
2026-02-20 14:27:22 +0100

    fix snack 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -3,4 +3,4 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
 
-console.log(names);
+const risultato = names.map(nome => `'${nome}'`).join(', ')
