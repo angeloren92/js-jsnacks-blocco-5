57d336ecd582f9e2bae9a016c24b29dd84b47a57
angeloren92 <angeloren92@gmail.com>
2026-02-20 18:33:24 +0100

    complete snack 5

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,27 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const risultato = nums.filter(num => num % 2 === 0)
+console.log(risultato)
+
+const risultato2 = nums.filter(num => {
+    if (num % 2 === 0) {
+        return num;
+    }
+})
+console.log(risultato2)
+
+
+//Prova riscotruziuone filtro manuale per ripassare la logica
+function myFilter(arr) {
+    result = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            result.push(arr[i])
+        }
+    }
+    return result;
+}
+const risultato3 = myFilter(nums)
+console.log(risultato3)
