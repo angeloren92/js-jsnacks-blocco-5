7667faa08351f2700f9bcd789d54a53c3dbaf9cd
angeloren92 <angeloren92@gmail.com>
2026-02-20 15:09:35 +0100

    complete snack 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,7 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+
+const risultato = posts.map(({author}) => author )
+console.log(risultato)
