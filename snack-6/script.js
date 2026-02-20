4688ded0ab2f570e6da4bcbb55879eb0f07c5816
angeloren92 <angeloren92@gmail.com>
2026-02-20 18:41:27 +0100

    complete snack 6

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,7 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+const zucchineLunghe = zucchine.filter(({length}) => (length >= 15))
+const zucchineCorte = zucchine.filter(({length}) => (length < 15))
+console.log(zucchineLunghe, zucchineCorte)
--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,6 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+const result = students.find(({id}) => (id === 2))
+console.log(students, result)
