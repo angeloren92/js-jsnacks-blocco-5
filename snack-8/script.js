41ac3a8212fe5639e8fbf528ce8abbcf7f8de135
angeloren92 <angeloren92@gmail.com>
2026-02-20 18:55:28 +0100

    complete snack 8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,6 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const risultato = students.find(({name}) => (name === 'Marco Lanci'))
+console.log(risultato.class)
